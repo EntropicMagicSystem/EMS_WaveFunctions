bcf099ea52d93aad577db2570a6dd36f2d92ceb0
NieTsuki <nie.0x6708@gmail.com>
2024-06-17 13:08:59 +0800

    Implement time

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 let scaleSlider;
 
+const timeSpeed = 5;
+let time = 0;
+
 function setupCanvasParams() {
     scaleSlider = createSlider(1, 500, 100);
     scaleSlider.position(20, 20);
@@ -27,10 +30,15 @@ function draw() {
     translate(width / 2, height * 0.7);
     beginShape();
 
-    let x = -width / 2 / scale;
-    while (x * scale < width / 2) {
-        vertex(x * scale, sin(x) * scale);
-        x += 0.01;
+    for (
+        let x = (-width / 2 / scale) - (time / scale);
+        x * scale < (width / 2) - time;
+        x += 0.01
+    ) {
+        vertex(
+            (x * scale) + time,
+            sin(x) * scale
+        );
     }
 
     endShape();
@@ -41,4 +49,6 @@ function draw() {
     stroke(255);
     textAlign(LEFT, CENTER);
     text(`Scale: ${scale / 100}x`, 150, 20);
+
+    time += timeSpeed;
 }
